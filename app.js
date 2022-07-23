feea9255199f6de64d6ef5f04da700cb46a24321
balance2777 <strong777759@gmail.com>
2022-07-23 19:20:28 +0900

    やっぱりもどす

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
-//expressモジュールの読み込み
-const express = require('express')
-//expressのインスタンス化
-const app = express()
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
-//8080番ポートでサーバーを待ちの状態にする。
-//またサーバーが起動したことがわかるようにログを出力する
-app.listen(process.env.PORT || 3000, () => {
-  console.log("サーバー起動中");
+
+
+let count = 0;
+
+
+
+io.on('connection', function(socket) {
+console.log("接続しました")
+
+socket.on("idid",(idid,idid2)=>{
+console.log(idid)
+console.log(idid2)
+
+io.emit("rrr", idid,idid2);
+
+})
+
+socket.on("own_piece",(x,y,own_piece_id)=>{
+
+io.emit("own_piece_response", x,y,own_piece_id);
+
+})
+
+})
+
+
+//viewエンジンをejsであることを設定
+app.set("view engine", "ejs");
+
+
+
+app.get('/', function(req, res){
+count++
+res.render("landing", {count: count});
 });
 
-//GETリクエストの設定
-//'/get'でアクセスされた時に、JSONとログを出力するようにする
-app.get('/', (req, res)=> {
-    console.log('GETリクエストを受け取りました')
-    res.send("復活");
-})
+
+
+server.listen(process.env.PORT || 3000, () => console.log('app listening on port 3000!'))
+
