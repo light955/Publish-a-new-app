108123f4f5cf588afcf01bdf8b8278a82012b213
balance2777 <strong777759@gmail.com>
2024-06-19 13:32:29 +0900

    なってほしい

---
+++ aaa.js
@@ -0,0 +1,19 @@
+function createCounter() {
+    let count = 0; // 初期値
+  
+    return function() {
+      const currentCount = count; // 以前のカウントを別の変数に保存
+      count++; // カウントを増やす
+      return currentCount; // 以前のカウントを返す
+    };
+  }
+  
+  const counter = createCounter();
+  
+  console.log(counter()); // 1
+  console.log(counter()); // 2
+  console.log(counter()); // 3
+  console.log(counter()); // 1
+  console.log(counter()); // 1
+
+  
--- app.js
+++ app.js
@@ -5,39 +5,56 @@ const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
 
-let count = 0;
+let count = 0;      //ゲームのカウント
+let playercount = 0;    //playerのcount
 
 let namesave = [];
 let playerData = {};
 
 io.on('connection', function (socket) {
-    if(count == 1){
+    playercount++
+    if (playercount == 1) {
         playerData[socket.id] = 1
     }
-    if(count == 2){
+    if (playercount == 2) {
         playerData[socket.id] = 2
     }
+    //こちらは観戦用
+    if (playercount > 2) {
+        playerData[socket.id] = 3
+    }
     socket.on('page_loaded', function (data) {
-        namesave.push(data.message)
-        // 必要な処理をここで行う
+        if (playercount <= 2) {
+            //いつか下記を適用させる
+            //namesave.push({ id: socket.id, name: data.message });
+            namesave.push(data.message);
+            console.log(namesave)
+        }
         io.emit("formdata", namesave);
     });
 
-    socket.on("ban_click", (ban_x, ban_y,turn) => {
-        if(!(playerData[socket.id] === turn)){
+    socket.on("ban_click", (ban_x, ban_y, turn) => {
+        if (!(playerData[socket.id] === turn)) {
             console.log("あなたのターンではないよ!!")
             return
         }
         io.emit("ban_click_res", ban_x, ban_y);
     })
 
-    socket.on("own_piece", (x, y, own_piece_id,turn) => {
-        if(!(playerData[socket.id] === turn)){
+    socket.on("own_piece", (x, y, own_piece_id, turn) => {
+        if (!(playerData[socket.id] === turn)) {
             console.log("あなたのターンではないよ!!")
             return
         }
+        if(playerData[socket.id]==1 && own_piece_id=="field_1"||playerData[socket.id]==2&& own_piece_id=="field_2"){
+            io.emit("own_piece_response", x, y, own_piece_id);
+        }else{
+            console.log("あなたの駒ではないよ!")
+            return    
+        }
 
-        io.emit("own_piece_response", x, y, own_piece_id);
+
+
     })
 
     socket.on("button", () => {
@@ -49,6 +66,22 @@ io.on('connection', function (socket) {
         console.log("button2を送信するばい")
         io.emit("button_response2")
     })
+
+    socket.on('disconnect', function () {
+        // countの更新
+        playercount--;
+        namesave.splice(playerData[socket.id]-1, 1);
+        // playerDataから該当のsocket.idを削除
+        delete playerData[socket.id];
+
+        // namesaveから該当する名前を削除（より複雑）
+        // namesave配列の更新方法については下記参照
+
+        // 全てのクライアントが切断された場合の処理
+        // if (io.engine.clientsCount === 0) {
+        //     count = 0; // ゲーム状態のリセットなど
+        // }
+    });
 })
 
 //ミドルウェアの設定
@@ -67,8 +100,46 @@ app.get('/', function (req, res) {
 app.post('/login', function (req, res) {
     //値を受け取りたい
     count++
-    res.render("landing", { count: count,username: req.body.username});
+    res.render("landing", { count: count, username: req.body.username });
 });
 
 server.listen(process.env.PORT || 3000, () => console.log('app listening on port 3000!'))
+
+
+
+
+
+
+
+
+
+
+
+
+//練習だよ
+
+
+// const people = [
+//     { name: "Alice", age: 25 },
+//     { name: "Bob", age: 16 },
+//     { name: "Charlie", age: 30 }
+//   ];
+  
+//   const adults = people.filter(function(person) {
+//     console.log(person)
+//     return person.age >= 18;
+//   });
+  
+
+
+let aaa = ["こんにちは", "こんばんは", "おはよう"];
+
+// インデックス1の要素を取り除く
+aaa.splice(2, 1);
+
+console.log(aaa);
+
+
+  
+  
 
